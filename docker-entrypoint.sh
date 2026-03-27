80972bca866584fcb8fb07985dfd3651ddc55dba
kiskey <59226371+kiskey@users.noreply.github.com>
2026-03-26 23:48:53 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-set -e
-
-# If config file does not exist in mounted volume, copy default
 if [ ! -f /root/configs/config.yaml ]; then
     echo "No config.yaml found, copying default..."
-    cp /root/default-config.yaml /root/configs/config.yaml
+    cp /root/configs/default_config.yaml /root/configs/config.yaml
 fi
-
-# Run the agent
-exec "$@"
+exec ./agent --web
