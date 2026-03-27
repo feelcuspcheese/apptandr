e4305e803bd5a820844c30262088e9f0481cf84e
kiskey <59226371+kiskey@users.noreply.github.com>
2026-03-27 16:52:55 -0700

    Update config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,40 +8,40 @@ import (
 type Museum struct {
     Name     string `mapstructure:"name"`
     Slug     string `mapstructure:"slug"`
-    MuseumID string `mapstructure:"museum_id"`
+    MuseumID string `mapstructure:"museumid"`
 }
 
 type Site struct {
     Name                 string            `mapstructure:"name"`
-    BaseURL              string            `mapstructure:"base_url"`
-    AvailabilityEndpoint string            `mapstructure:"availability_endpoint"`
+    BaseURL              string            `mapstructure:"baseurl"`
+    AvailabilityEndpoint string            `mapstructure:"availabilityendpoint"`
     Digital              bool              `mapstructure:"digital"`
     Physical             bool              `mapstructure:"physical"`
     Location             string            `mapstructure:"location"`
-    BookingLinkSelector  string            `mapstructure:"booking_link_selector"`
-    LoginForm            LoginFormConfig   `mapstructure:"login_form"`
-    BookingForm          BookingFormConfig `mapstructure:"booking_form"`
-    SuccessIndicator     string            `mapstructure:"success_indicator"`
+    BookingLinkSelector  string            `mapstructure:"bookinglinkselector"`
+    LoginForm            LoginFormConfig   `mapstructure:"loginform"`
+    BookingForm          BookingFormConfig `mapstructure:"bookingform"`
+    SuccessIndicator     string            `mapstructure:"successindicator"`
     Museums              map[string]Museum `mapstructure:"museums"`
-    PreferredSlug        string            `mapstructure:"preferred_slug"`
+    PreferredSlug        string            `mapstructure:"preferredslug"`
 }
 
 type LoginFormConfig struct {
-    UsernameField       string `mapstructure:"username_field"`
-    PasswordField       string `mapstructure:"password_field"`
-    SubmitButton        string `mapstructure:"submit_button"`
-    CSRFSelector        string `mapstructure:"csrf_token_selector"`
-    Username            string `mapstructure:"username"`
-    Password            string `mapstructure:"password"`
-    Email               string `mapstructure:"email"`
-    AuthIDSelector      string `mapstructure:"auth_id_selector"`
-    LoginURLSelector    string `mapstructure:"login_url_selector"`
+    UsernameField    string `mapstructure:"usernamefield"`
+    PasswordField    string `mapstructure:"passwordfield"`
+    SubmitButton     string `mapstructure:"submitbutton"`
+    CSRFSelector     string `mapstructure:"csrfselector"`
+    Username         string `mapstructure:"username"`
+    Password         string `mapstructure:"password"`
+    Email            string `mapstructure:"email"`
+    AuthIDSelector   string `mapstructure:"authidselector"`
+    LoginURLSelector string `mapstructure:"loginurlselector"`
 }
 
 type BookingFormConfig struct {
-    ActionURL  string            `mapstructure:"action_url"`
+    ActionURL  string            `mapstructure:"actionurl"`
     Fields     []FormFieldConfig `mapstructure:"fields"`
-    EmailField string            `mapstructure:"email_field"`
+    EmailField string            `mapstructure:"emailfield"`
 }
 
 type FormFieldConfig struct {
@@ -53,28 +53,28 @@ type FormFieldConfig struct {
 
 type ScheduledRun struct {
     ID         string    `mapstructure:"id"`
-    SiteKey    string    `mapstructure:"site_key"`
-    MuseumSlug string    `mapstructure:"museum_slug"`
-    DropTime   time.Time `mapstructure:"drop_time"`
+    SiteKey    string    `mapstructure:"sitekey"`
+    MuseumSlug string    `mapstructure:"museumslug"`
+    DropTime   time.Time `mapstructure:"droptime"`
     Mode       string    `mapstructure:"mode"`
 }
 
 type AppConfig struct {
-    Sites            map[string]Site `mapstructure:"sites"`
-    ActiveSite       string          `mapstructure:"active_site"`
-    Mode             string          `mapstructure:"mode"`
-    PreferredDays    []string        `mapstructure:"preferred_days"`
-    StrikeTime       string          `mapstructure:"strike_time"`
-    CheckWindow      time.Duration   `mapstructure:"check_window"`
-    CheckInterval    time.Duration   `mapstructure:"check_interval"`
-    PreWarmOffset    time.Duration   `mapstructure:"pre_warm_offset"`
-    NtfyTopic        string          `mapstructure:"ntfy_topic"`
-    MaxWorkers       int             `mapstructure:"max_workers"`
-    RequestJitter    time.Duration   `mapstructure:"request_jitter"`
-    MonthsToCheck    int             `mapstructure:"months_to_check"`
-    ScheduledRuns    []ScheduledRun  `mapstructure:"scheduled_runs"`
-    RestCycleChecks  int             `mapstructure:"rest_cycle_checks"`   // number of checks before a rest
-    RestCycleDuration time.Duration  `mapstructure:"rest_cycle_duration"` // duration of rest
+    Sites             map[string]Site `mapstructure:"sites"`
+    ActiveSite        string          `mapstructure:"active_site"`
+    Mode              string          `mapstructure:"mode"`
+    PreferredDays     []string        `mapstructure:"preferred_days"`
+    StrikeTime        string          `mapstructure:"strike_time"`
+    CheckWindow       time.Duration   `mapstructure:"check_window"`
+    CheckInterval     time.Duration   `mapstructure:"check_interval"`
+    PreWarmOffset     time.Duration   `mapstructure:"pre_warm_offset"`
+    NtfyTopic         string          `mapstructure:"ntfy_topic"`
+    MaxWorkers        int             `mapstructure:"max_workers"`
+    RequestJitter     time.Duration   `mapstructure:"request_jitter"`
+    MonthsToCheck     int             `mapstructure:"months_to_check"`
+    ScheduledRuns     []ScheduledRun  `mapstructure:"scheduled_runs"`
+    RestCycleChecks   int             `mapstructure:"rest_cycle_checks"`
+    RestCycleDuration time.Duration   `mapstructure:"rest_cycle_duration"`
 }
 
 func LoadConfig(path string) (*AppConfig, error) {
@@ -87,6 +87,7 @@ func LoadConfig(path string) (*AppConfig, error) {
     if err := viper.Unmarshal(&cfg); err != nil {
         return nil, err
     }
+
     if cfg.MonthsToCheck == 0 {
         cfg.MonthsToCheck = 2
     }
